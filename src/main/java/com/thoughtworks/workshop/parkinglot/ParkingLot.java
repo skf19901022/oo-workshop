141935a84508a4c73784fdb705f15e1a5b6114ed
Kefan Shen <shenkefan1990@gmail.com>
2018-12-24 13:06:55 +0800

    [kefan&chengxin] add the parking function for the parkingLot. When there is no available carport, we will park failed.

--- src/main/java/com/thoughtworks/workshop/parkinglot/ParkingLot.java
+++ src/main/java/com/thoughtworks/workshop/parkinglot/ParkingLot.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.workshop.parkinglot;
 
+import com.thoughtworks.workshop.parkinglot.exception.ParkingException;
+
 public class ParkingLot {
 
   private int availableSeatCount;
@@ -10,7 +12,7 @@ public class ParkingLot {
 
   public Ticket parking(Car car) {
     if (availableSeatCount == 0) {
-      return null;
+      throw new ParkingException();
     }
     availableSeatCount--;
     return new Ticket(car.getInfo());
--- src/test/java/com/thoughtworks/workshop/parkinglot/ParkingLotTest.java
+++ src/test/java/com/thoughtworks/workshop/parkinglot/ParkingLotTest.java
@@ -1,18 +1,27 @@
 package com.thoughtworks.workshop.parkinglot;
 
 
+import com.thoughtworks.workshop.parkinglot.exception.ParkingException;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ParkingLotTest {
 
   @Test
-  void shouldReturnTrueWhenParkingCarGivenParkingLotHaveAvailableSeat() {
+  void shouldReturnTicketWhenParkingCarGivenParkingLotHaveAvailableSeat() {
     Car car = new Car("1");
     ParkingLot parkingLot = new ParkingLot(1);
 
-    assertNotNull(parkingLot.parking(car));
+    assertEquals("1", parkingLot.parking(car).getCarInfo());
+  }
 
+  @Test
+  void shouldReturnExceptionWhenParkingCarGivenParkingLotIsFull() {
+    Car car = new Car("1");
+    ParkingLot parkingLot = new ParkingLot(0);
+
+    assertThrows(ParkingException.class, () -> parkingLot.parking(car));
   }
 }
