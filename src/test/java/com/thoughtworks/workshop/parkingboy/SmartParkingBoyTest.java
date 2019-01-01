e7902f27a4b6c453efa45ddac2dc073387c32ba8
Kefan Shen <shenkefan1990@gmail.com>
2019-01-01 18:22:27 +0800

    [kefan&chengxin] Task4 Park in Lot 1 Given Lot 1 and 2 is full.

--- src/test/java/com/thoughtworks/workshop/parkingboy/SmartParkingBoyTest.java
+++ src/test/java/com/thoughtworks/workshop/parkingboy/SmartParkingBoyTest.java
@@ -3,10 +3,12 @@ package com.thoughtworks.workshop.parkingboy;
 import com.thoughtworks.workshop.parkinglot.Car;
 import com.thoughtworks.workshop.parkinglot.ParkingLot;
 import com.thoughtworks.workshop.parkinglot.Ticket;
+import com.thoughtworks.workshop.parkinglot.exception.ParkingLotIsFullException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SmartParkingBoyTest {
 
@@ -59,5 +61,23 @@ class SmartParkingBoyTest {
     assertNotNull(ticket);
 
     assertSame(car, parkingLot1.pick(ticket));
+  }
+
+  @Test
+  void shouldThrowParkingLotExceptionWhenSmartBoyParkCarGivenParkingLot1And2IsFull() {
+    ParkingLot parkingLot1 = new ParkingLot(1);
+    ParkingLot parkingLot2 = new ParkingLot(1);
+
+    SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
+    smartParkingBoy.manage(parkingLot1);
+    smartParkingBoy.manage(parkingLot2);
+
+    Car toyato = new Car();
+    Car honda = new Car();
+    smartParkingBoy.park(toyato);
+    smartParkingBoy.park(honda);
+
+    Car porsche = new Car();
+    assertThrows(ParkingLotIsFullException.class, () -> smartParkingBoy.park(porsche));
   }
 }
